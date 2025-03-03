eca1094624b88879882e8361cdbd86c26ef5c624
joachim <ageron.joachim@gmail.com>
2025-03-03 21:12:30 +0100

    ✨ add: update button to navigate to sign in page
    Enhance the home page button functionality to redirect users
    to the sign-in page using the router.

--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Button } from "@heroui/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Button>Click me</Button>
+        <Button onPress={()=> router.push('/auth/sigin')}>go to sign in</Button>
       </main>
     </div>
   );
