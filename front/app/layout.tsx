59ba93b8e9fdf44c6d0d6a09700244d6ce2a7c4c
Louis-Cauvet <louiscauvet8@gmail.com>
2025-03-05 12:13:02 +0100

    ✨ add: front on homepage

--- front/app/layout.tsx
+++ front/app/layout.tsx
@@ -7,17 +7,44 @@ export const metadata: Metadata = {
   description: "Lokify is the best loker app",
 };
 
+const teamMembers = [
+  { name: "Joachim Ageron dit Blanc", github: "https://github.com/joachimageron" },
+  { name: "Lisa Michallon", github: "https://github.com/lmichallon" },
+  { name: "Hugo Duperthuy", github: "https://github.com/heavenProx" },
+  { name: "Louis Cauvet", github: "https://github.com/Louis-Cauvet" },
+];
+
+function Footer() {
+  return (
+    <footer className="flex flex-col items-center gap-2 p-2">
+      <p>Lockify &copy; 2025</p>
+      <ul className="flex flex-col sm:flex-row">
+        {teamMembers.map((member, index) => (
+          <li key={index} className="flex justify-center items-center wrap">
+            <a href={member.github} target="_blank" rel="noopener noreferrer" className="underline text-center hover:text-blue-700">
+              {member.name}
+            </a>
+            {index < teamMembers.length - 1 && <span className="mx-3 hidden sm:block"> - </span>}
+          </li>
+        ))}
+      </ul>
+    </footer>
+  );
+}
+
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="fr">
       <body>
         <Providers>
         {children}
         </Providers>
+        <Footer />
       </body>
     </html>
   );
--- front/app/page.tsx
+++ front/app/page.tsx
@@ -1,14 +1,42 @@
 "use client";
 import { Button } from "@heroui/react";
 import { useRouter } from "next/navigation";
+import { LockClosedIcon, ClockIcon, BellIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   const router = useRouter();
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Button color="primary" onPress={()=> router.push('/auth/signin')}>go to signin in</Button>
-      </main>
-    </div>
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100 px-4 bg-home-img">
+    
+      <div className="max-w-3xl text-center bg-white shadow-lg rounded-2xl p-8">
+        <h1 className="text-4xl font-bold text-gray-900 mb-4">Bienvenue sur Lockify !</h1>
+        
+        <p className="text-gray-700 text-lg mb-8">
+          Réservez un casier en ligne en toute simplicité.<br></br>
+          Avec Lockify, sécurisez vos affaires sans stress et profitez de votre temps libre !
+        </p>
+
+        <ul className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-9">
+          <li className="flex flex-col items-center">
+            <LockClosedIcon className="h-10 w-10 text-blue-500 mb-2" />
+            <p className="font-semibold text-gray-800">Sécurisé</p>
+            <p className="text-sm text-gray-600">Vos affaires restent en sécurité 24/7.</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <ClockIcon className="h-10 w-10 text-blue-500 mb-2" />
+            <p className="font-semibold text-gray-800">Flexible</p>
+            <p className="text-sm text-gray-600">Choisissez la taille et réservez pour la durée qui vous convient.</p>
+          </li>
+          <li className="flex flex-col items-center">
+            <BellIcon className="h-10 w-10 text-blue-500 mb-2" />
+            <p className="font-semibold text-gray-800">Notifications</p>
+            <p className="text-sm text-gray-600">Recevez un rappel avant expiration.</p>
+          </li>
+        </ul>
+
+        <Button className="bg-blue-600 hover:bg-blue-700 text-white px-6 py-3 text-lg rounded-lg" 
+        onPress={()=> router.push('/auth/signin')}>Je réserve mon casier</Button>
+      </div>
+    </main>
   );
 }
